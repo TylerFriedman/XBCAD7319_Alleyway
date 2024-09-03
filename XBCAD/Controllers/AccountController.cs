e80d92cdcabb2c057e51dc1ae7bc08f28490b3a0
TylerFriedman <st1016340@vcconnect.edu.za>
2024-09-03 16:52:00 +0200

    Local repo

---
+++ XBCAD/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace XBCAD.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password, bool rememberMe)
+        {
+            // Add your login logic here
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
