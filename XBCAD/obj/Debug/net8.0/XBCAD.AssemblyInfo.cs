94ba64646869c84e6442a66726fa5641c817e345
TylerFriedman <st1016340@vcconnect.edu.za>
2024-09-03 19:38:27 +0200

    added a glow for nav bar

--- XBCAD/obj/Debug/net8.0/XBCAD.AssemblyInfo.cs
+++ XBCAD/obj/Debug/net8.0/XBCAD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XBCAD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99bd152989834ee4fcccfff65094e0fe8bc57d06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e000d18847272bbc4413eb870ba7a9689d39ee1")]
 [assembly: System.Reflection.AssemblyProductAttribute("XBCAD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XBCAD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
