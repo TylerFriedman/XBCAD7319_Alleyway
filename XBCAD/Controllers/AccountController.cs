d069af03fd59cc57de6e4b251fc16d25567b494c
TylerFriedman <st1016340@vcconnect.edu.za>
2024-09-03 17:14:39 +0200

    login and register looks done

--- XBCAD/Controllers/AccountController.cs
+++ XBCAD/Controllers/AccountController.cs
@@ -1,24 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using XBCAD.ViewModels;
 
 namespace XBCAD.Controllers
 {
     public class AccountController : Controller
     {
+        // GET: Account/Login
         public IActionResult Login()
         {
             return View();
         }
 
         [HttpPost]
-        public IActionResult Login(string username, string password, bool rememberMe)
+        public IActionResult Login(LoginViewModel model)
         {
-            // Add your login logic here
-            return RedirectToAction("Index", "Home");
+            if (ModelState.IsValid)
+            {
+                // Handle login logic here
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
         }
 
+        // GET: Account/Register
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Handle registration logic here
+                return RedirectToAction("Login");
+            }
+            return View(model);
         }
     }
 }
