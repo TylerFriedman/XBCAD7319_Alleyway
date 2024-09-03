31dbbc1157bebeb05b1e118b2b117083b700cdac
UndeadRonin99 <ST10046014@vcconnect.edu.za>
2024-09-04 01:32:33 +0200

    pushing update

--- XBCAD/obj/Debug/net8.0/XBCAD.AssemblyInfo.cs
+++ XBCAD/obj/Debug/net8.0/XBCAD.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XBCAD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f189ee58f2d344235f1947a1aaaf609cf789647e")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+064347fd6cae739aa508072cba689579669e124a")]
+>>>>>>> 4affbe48c3d9a29910988256bb436f986637fe52
 [assembly: System.Reflection.AssemblyProductAttribute("XBCAD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XBCAD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
